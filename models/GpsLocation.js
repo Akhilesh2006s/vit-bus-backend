b02bb0f79037a67aec20926fc2a8f329e3efce80
Akhilesh2006s <155376342+Akhilesh2006s@users.noreply.github.com>
2025-08-30 00:51:46 +0530

    Update GpsLocation.js

--- models/GpsLocation.js
+++ models/GpsLocation.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const gpsSchema = new mongoose.Schema({
-    route: String,
-    lat: Number,
-    lon: Number,
-    stopName: String, // optional if only tracking
-    timestamp: { type: Date, default: Date.now }
+  route: { type: String, required: true },
+  lat: { type: Number, required: true },
+  lon: { type: Number, required: true },
+  stopName: { type: String },
+  timestamp: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("GpsLocation", gpsSchema);
